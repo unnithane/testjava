5fafe925ead1cbb78c6b95a5834a689c0e1aeb3f
unnithane <unnithane88@gmail.com>
2017-06-14 14:01:33 +0530

    hello word

---
+++ amith/src/amith/test.java
@@ -0,0 +1,13 @@
+package amith;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello word");
+		
+         
+	}
+
+}
