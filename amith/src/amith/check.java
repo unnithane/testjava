b9a525ab9b3a161a3171ad605eb26b4b04ba4cf6
unnithane <unnithane88@gmail.com>
2017-06-15 15:37:48 +0530

    greater or shorter

---
+++ amith/src/amith/check.java
@@ -0,0 +1,21 @@
+package amith;
+
+public class check {
+
+	public static void main(String[] args) {
+		
+		int x = 20;
+		int y = 30;
+		if(x==y){
+			System.out.println("both are equal");
+			
+		}else if(x>y){
+			System.out.println("x is greater than y");
+		}else{
+			System.out.println("y is greater than x");
+		}
+		// TODO Auto-generated method stub
+
+	}
+
+}
